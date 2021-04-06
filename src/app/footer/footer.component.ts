86f748c9e0662a686bfc096639ddde31e65947d0
unknown <reifenberg.57@gmail.com>
2021-04-06 14:49:55 -0500

    Basic Navbar, Imported Tailwind, Basic Footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  currentYear: number = new Date().getFullYear();
 
   constructor() { }
 
   ngOnInit(): void {
+    this.currentYear;
   }
 
 }

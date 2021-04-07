e3bc7f56c07468216aa5155c90014ac8a218445b
unknown <reifenberg.57@gmail.com>
2021-04-07 16:26:58 -0500

    Introductory Component In Progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule

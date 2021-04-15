999fb9eac8cbc1a995e19e2a4a6ad3740d0b3a26
unknown <reifenberg.57@gmail.com>
2021-04-15 14:47:19 -0500

    Fixed Project Cards, Updated Links

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,11 +10,30 @@ import { Project } from './project.model'
 export class ProjectsComponent implements OnInit {
 
   projects: Project[] = [
-    new Project('NASA - Daily Picture', 'This is a Test', 'https://www.jpl.nasa.gov/spaceimages/images/largesize/PIA18905_hires.jpg','https://tomreifenberg.github.io/vanilla-apod/'),
-    new Project('Abstract Art Canvas', 'This is a Test', 'https://cdn.pixabay.com/photo/2015/01/31/05/05/background-618226_960_720.png','https://tomreifenberg.github.io/draw/program.html'),
-    new Project('Meteor Dodge!', 'This is a Test', 'https://www.universetoday.com/wp-content/uploads/2011/11/Leonids-NASA.jpg','https://tomreifenberg.github.io/draw/program.html'),
-    new Project('Image Carousel', 'This is a Test', 'https://i.imgur.com/toD7trX.jpg','https://tomreifenberg.github.io/draw/program.html'),
-  
+    new Project(
+      'NASA - Daily Picture', 
+      'Take a journey through decades of NASA imagery', 
+      'https://www.jpl.nasa.gov/spaceimages/images/largesize/PIA18905_hires.jpg',
+      'https://tomreifenberg.github.io/vanilla-apod/'
+      ),
+    new Project(
+      'Meteor Dodge!', 
+      'Navigate your heroic square to safety in a meteor downpour', 
+      'https://www.universetoday.com/wp-content/uploads/2011/11/Leonids-NASA.jpg',
+      'https://tomreifenberg.github.io/game/'
+      ),
+    new Project(
+      'Image Carousel', 
+      'Cycle through images of my good friends, Lenny and Glen', 
+      'https://i.imgur.com/toD7trX.jpg',
+      'https://tomreifenberg.github.io/carousel/'
+      ),
+    new Project(
+      'Abstract Art Canvas', 
+      'Paint bizarre and wonderful art within your browser', 
+      'http://www.mepixels.com/cache/af1d45ba/abstract-geometric-background-1140x1140-S5ToJTPV4.jpeg',
+      'https://tomreifenberg.github.io/draw/program.html'
+      ),  
 
   ];
 
